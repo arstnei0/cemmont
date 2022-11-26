967d91c710b851fb542f9c58eba9be024df0a158
Zihan Chen <me@zihan.ga>
2022-11-26 09:14:08 +0800

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<slot />
+</template>
+
+<script setup lang="ts">
+import { createPinia } from "pinia"
+
+const nuxtApp = useNuxtApp()
+const vueApp = nuxtApp.vueApp
+const pinia = createPinia()
+vueApp.use(pinia)
+</script>
